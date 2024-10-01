fe5d915832718a5ab4e487897a9b70773b331651
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2024-09-30 17:00:51 -0700

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def initialize_weights(module):
+    if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+        nn.init.kaiming_uniform_(module.weight, nonlinearity='relu')
+        if module.bias is not None:
+            module.bias.data.fill_(0.0)
+
+# Define the SurgicalEquipmentNet architecture
+class SurgicalEquipmentNet(nn.Module):
+    def __init__(self, num_classes=5):
+        super(SurgicalEquipmentNet, self).__init__()
+
+        # First Convolutional Block
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+
+        # Second Convolutional Block
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
+
+        # Third Convolutional Block
+        self.conv5 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.conv6 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(512)
+
+        # Global Average Pooling
+        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, num_classes)
+
+        # Dropout layer for regularization
+        self.dropout = nn.Dropout(0.5)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # First Convolutional Block
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # Second Convolutional Block
+        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.relu(self.bn4(self.conv4(x)))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # Third Convolutional Block
+        x = self.relu(self.bn5(self.conv5(x)))
+        x = self.relu(self.bn6(self.conv6(x)))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # Global Average Pooling and Flatten
+        x = self.global_pool(x)
+        x = x.view(x.size(0), -1)
+
+        # Fully Connected Layers with Dropout
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+
+        return x
+
+
+# Example instantiation for a specified number of classes
+model = SurgicalEquipmentNet(num_classes=5)
+print(model)
